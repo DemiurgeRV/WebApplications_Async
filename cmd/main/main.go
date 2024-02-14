ab10d3dc79d8a1986f49f924905d6f1e25baa23b
Vladislav Rybin <rybinvladislav@mail.ru>
2024-02-14 17:09:33 +0300

    sharing images

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,44 +4,72 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"math/rand"
+	"mime/multipart"
 	"net/http"
 	"time"
 )
 
-func randomStatus() int {
+func smoothEffect() int {
 	time.Sleep(10 * time.Second)
 	return rand.Intn(2)
 }
 
-func SendStatus(pk string, url string) {
-	result := randomStatus()
+func SendImage(id string, url string) {
+	result := smoothEffect()
 	fmt.Println(result)
-	data := []byte(`{"name": "John", "age": 30}`)
-
-	req, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(data))
+	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Println("Error create request:", err)
-		return
-	}
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending data:", err)
+		fmt.Println("Error getting image:", err)
 		return
 	}
 	defer resp.Body.Close()
-	fmt.Println("Status sent successfully for pk:", pk)
+
+	// Прочитаем полученное изображение
+	imgData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading image data:", err)
+		return
+	}
+
+	// Создаем FormData и добавляем изображение
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile("image", ""+id+".png")
+	if err != nil {
+		fmt.Println("Error creating form file:", err)
+		return
+	}
+	part.Write(imgData)
+	writer.Close()
+
+	// Отправляем FormData на сервер Django
+	resp2, err := http.NewRequest("PUT", "http://localhost:8000/api/orders/"+id+"/image/update/", body)
+	if err != nil {
+		fmt.Println("Error create PUT request:", err)
+		return
+	}
+	resp2.Header.Set("Content-Type", writer.FormDataContentType())
+
+	client := &http.Client{}
+	resp3, err := client.Do(resp2)
+	if err != nil {
+		fmt.Println("Error sending PUT request:", err)
+		return
+	}
+
+	defer resp3.Body.Close()
+
+	fmt.Println("Image updated in Orders model")
 }
 
 func main() {
 	router := gin.Default()
 	router.POST("/edit_image/", func(c *gin.Context) {
 		id := c.PostForm("id")
-		go SendStatus(id, "http://localhost:8000/api/edit_image/"+id+"/")
-		c.JSON(http.StatusOK, gin.H{"message": "Status update initiated"})
+		go SendImage(id, "http://localhost:8000/api/orders/"+id+"/image/")
+		c.JSON(http.StatusOK, gin.H{"message": "Image retrieval and update initiated"})
 	})
 	router.Run(":8080")
 }
